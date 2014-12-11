a5375a0580ec9b402afe6f18773b237703723957
Atul9 <atul1bhosale@gmail.com>
2014-12-11 20:40:25 +0530

    Symbol and string object_id

---
+++ p039symbol.rb
@@ -0,0 +1,7 @@
+# p039symbol.rb
+puts "string".object_id
+puts "string".object_id
+puts :symbol.object_id
+puts :symbol.object_id
+# Although both the strings are equal they have different object ids
+# Both the symbols have same object id
