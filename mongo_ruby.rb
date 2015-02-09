243604d63038b7e4a4572f8f9b06bd276a2bbdf4
Atul Bhosale <atul@joshsoftware.com>
2015-02-09 13:43:28 +0530

    Add comments to mongo_ruby

--- mongo_ruby.rb
+++ mongo_ruby.rb
@@ -1,12 +1,16 @@
-require 'rubygems'
-require 'mongo'
-require 'bson'
+#require 'rubygems'
+require 'mongo' #Use mongo gem via require Kernel method
+#require 'bson'
 #require 'bson_ext'
-include Mongo
-client = MongoClient.new
+include Mongo #Include Mongo so that the Mongo classes are available without
+#having to specify the Mongo namespace.
+client = MongoClient.new #A MongoClient instance represents a connection to the Mongodb.
+#Default host is localhost and port number is 27017.
 db = client['mydb']
 coll = db['collection1']
 10.times{|i|
 coll.insert({:count =>i+1})}
 puts "There are #{coll.count} documents"
-puts coll.find.each{|doc| puts doc.inspect}
+coll.find.each{|doc| puts doc.inspect}
+
+puts client.database_names #list all the database names
