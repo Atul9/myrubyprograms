a5d190b2955b12ef12976720563557cb0b6a0b6a
Atul Bhosale <atul1bhosale@gmail.com>
2015-11-22 23:47:35 +0530

    Demo class with Binding

--- binding.rb
+++ binding.rb
@@ -1,1 +1,16 @@
 #learing binding class
+class Demo
+  def initialize(n)
+    @secret = n
+  end
+  def get_binding
+    return binding()
+  end
+end
+
+k1 = Demo.new(99)
+b1 = k1.get_binding
+k2 = Demo.new(-3)
+b2 = k2.get_binding
+puts "K1 #{k1.inspect}", "K2 #{k2.inspect}"
+eval("@secret", b1)
