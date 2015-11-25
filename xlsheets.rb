0fb2228ee61a184e2e60705b9daad34720663fec
Atul Bhosale <atul1bhosale@gmail.com>
2015-11-25 10:19:37 +0530

    Added spreadsheet gem

--- xlsheets.rb
+++ xlsheets.rb
@@ -1,4 +1,14 @@
-require 'rubyXL'
-workbook = RubyXL::Parser.parse("/home/atul/home/del/exl1.xlsx")
-puts workbook.worksheets[0]
-
+require 'write_xlsx'
+require 'spreadsheet'
+workbook = WriteXLSX.new('../ruby.xlsx')
+# Add a worksheet
+worksheet = workbook.add_worksheet
+worksheet.write(0,0,"No")
+worksheet.write(0,1,"Name")
+worksheet.write(0,2,"Class")
+(1..10).each do |j|
+  0.upto(2).each do |i|
+    worksheet.write(j, i, i + j )
+  end
+end
+workbook.close
